b8fcc4a3360cba70be955ac6020cd527ad90dbb2
LEDMOV <ledmov@outlook.com>
2024-10-27 00:21:29 +0200

    new file:   about.html new file:   contact.html modified:   index.html new file:   log.html new file:   moocs.html new file:   qualifications.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function toggleMenu() {
+    const navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
